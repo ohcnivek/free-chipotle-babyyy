17a88a662b0addd679e453d1086736d342590fc3
ohcnivek <64056046+ohcnivek@users.noreply.github.com>
2022-11-22 22:52:40 -0500

    figured out streaming & keeping http connection open

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 from twilio.rest import Client
 import requests
-
+import json
+import ast
 
 CHIPOTLE_PROMO_CODE = 'BOGO'
 CHIPOTLE_NUMBER = '+16786628359'
@@ -10,6 +11,12 @@ CHIPOTLE_NUMBER = '+16786628359'
 TWITTER_API_BASE_URL = 'https://api.twitter.com/2'
 TWITTER_API_STREAM_PARAM = '/tweets/search/stream'
 TWITTER_API_RULES_PARAM = '/rules'
+
+def pprint_json(calling_function, response):
+    print(("--- CALLING FUNCTION: {function} ---").format(function=calling_function))
+    result = ast.literal_eval(str(response.json()))
+    print(json.dumps(result, indent=4))
+
 
 class TwilioClient: 
     def __init__(self):
@@ -25,6 +32,7 @@ class TwilioClient:
 class TwitterDataStream:
     def __init__(self):
         load_dotenv()
+        self.session = requests.Session()
         self.bearer_token = os.environ['TWITTER_BEARER_TOKEN'] 
         self.headers = {"Content-Type": "application/json", "Authorization": "Bearer {token}".format(token=self.bearer_token)} 
 
@@ -34,21 +42,32 @@ class TwitterDataStream:
             TWITTER_API_BASE_URL + TWITTER_API_STREAM_PARAM + TWITTER_API_RULES_PARAM, 
             headers=self.headers,
             json=body)
-        print(response.json())
+        pprint_json("add_or_delete_rule", response)
 
     # get all rules enforced. 
     def get_rules(self):
         response = requests.get(
             TWITTER_API_BASE_URL + TWITTER_API_STREAM_PARAM + TWITTER_API_RULES_PARAM, 
             headers=self.headers)
+        pprint_json("get_rules", response)
 
-        print(response.json())
+    def get_tweet_stream(self):
+        print("Opening HTTP Connection...")
+        with self.session.request(
+            url=TWITTER_API_BASE_URL + TWITTER_API_STREAM_PARAM + '?tweet.fields=text', 
+            headers=self.headers, 
+            stream=True,
+            method='GET'
+        ) as resp:
+            print("--STATUS CODE-- {code}".format(code=resp.status_code))
+            if resp.status_code != 200:
+                print("error -- oops")
+            
+            # print out buffer
+            for line in resp.iter_lines():
+                if line:
+                    print(line)
 
-    def get_tweets(self):
-        response = requests.get(
-            TWITTER_API_BASE_URL + TWITTER_API_STREAM_PARAM + '?tweet.fields=text', 
-            headers=self.headers)
-        print(response.json())
 
 
 
@@ -58,28 +77,24 @@ def main():
     client = TwilioClient()
     client.send_message(CHIPTOLE_PROMO_CODE, '+13466372768', CHIPOTLE_NUMBER)
 
-
     # TESTING: ADDING STREAM RULE -- filtering by tweets from @certifiedaf
-    body = {"add": [{"value": "from:certifiedaf"}]}
+    body = {"add": [{"value": "from:certifiedaf", "tag": "tweets from @certifiedaf"}]}
     twitter_data_stream.add_or_delete_rule(body)
 
     # ADDING STREAM RULE -- filtering by tweets from @chiptoleTweets
-    # body = {"add": [{"value": "from:chiptoleTweets"}]}
-    # twitter_data_stream.add_or_delete_rule(body)
+    body = {"add": [{"value": "from:chiptoleTweets", "tag": "tweets from @chiptoleTweets"}]}
+    twitter_data_stream.add_or_delete_rule(body)
 
     # GET STREAM RULE 
     # ex. 'data': [{'id': '1594927913228443649', 'value': 'from:ChipotleTweets'}], 'meta': {'sent': '2022-11-22T05:40:53.281Z', 'result_count': 1}}
     twitter_data_stream.get_rules()
 
     # # DELETE STREAM RULE 
-    # body = {'delete': {'ids': ['1594927913228443649']}}
-    # twitter_data_stream.add_or_delete_rule(body)
+    body = {'delete': {'ids': ['1594932432863727617']}}
+    twitter_data_stream.add_or_delete_rule(body)
 
-    # todo: figure out stream
-    twitter_data_stream.get_tweets()
+    twitter_data_stream.get_tweet_stream()
 
-
-        
 
 
 if __name__ == "__main__":

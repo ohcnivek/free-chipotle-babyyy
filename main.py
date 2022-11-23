72ba4c4611c4012375766023ba3c58e7f735a9dd
ohcnivek <64056046+ohcnivek@users.noreply.github.com>
2022-11-22 22:57:04 -0500

    slihgt cleanin

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import requests
 import json
 import ast
 
+TEST_TWITTER_HANDLE = 'certifiedaf'
+
+CHIPOTLE_TWITTER_HANDLE = 'chiptoleTweets'
 CHIPOTLE_PROMO_CODE = 'BOGO'
 CHIPOTLE_NUMBER = '+16786628359'
 
@@ -36,7 +39,6 @@ class TwitterDataStream:
         self.bearer_token = os.environ['TWITTER_BEARER_TOKEN'] 
         self.headers = {"Content-Type": "application/json", "Authorization": "Bearer {token}".format(token=self.bearer_token)} 
 
-    # adding & deleting done in same method for conciseness.
     def add_or_delete_rule(self, body):
         response = requests.post(
             TWITTER_API_BASE_URL + TWITTER_API_STREAM_PARAM + TWITTER_API_RULES_PARAM, 
@@ -44,7 +46,6 @@ class TwitterDataStream:
             json=body)
         pprint_json("add_or_delete_rule", response)
 
-    # get all rules enforced. 
     def get_rules(self):
         response = requests.get(
             TWITTER_API_BASE_URL + TWITTER_API_STREAM_PARAM + TWITTER_API_RULES_PARAM, 
@@ -73,29 +74,25 @@ class TwitterDataStream:
 
 def main():
     twitter_data_stream = TwitterDataStream()
-
     client = TwilioClient()
     client.send_message(CHIPTOLE_PROMO_CODE, '+13466372768', CHIPOTLE_NUMBER)
 
     # TESTING: ADDING STREAM RULE -- filtering by tweets from @certifiedaf
-    body = {"add": [{"value": "from:certifiedaf", "tag": "tweets from @certifiedaf"}]}
+    body = {"add": [{"value": "from:{handle}", "tag": "tweets from @{handle}".format(TEST_TWITTER_HANDLE)}]}
     twitter_data_stream.add_or_delete_rule(body)
 
     # ADDING STREAM RULE -- filtering by tweets from @chiptoleTweets
-    body = {"add": [{"value": "from:chiptoleTweets", "tag": "tweets from @chiptoleTweets"}]}
+    body = {"add": [{"value": "from:{handle}", "tag": "tweets from @{handle}".format(CHIPOTLE_TWITTER_HANDLE)}]}
     twitter_data_stream.add_or_delete_rule(body)
 
-    # GET STREAM RULE 
+    # GET CURRENT STREAM RULE 
     # ex. 'data': [{'id': '1594927913228443649', 'value': 'from:ChipotleTweets'}], 'meta': {'sent': '2022-11-22T05:40:53.281Z', 'result_count': 1}}
     twitter_data_stream.get_rules()
 
     # # DELETE STREAM RULE 
-    body = {'delete': {'ids': ['1594932432863727617']}}
-    twitter_data_stream.add_or_delete_rule(body)
-
+    # body = {'delete': {'ids': ['1594932432863727617']}}
+    # twitter_data_stream.add_or_delete_rule(body)
     twitter_data_stream.get_tweet_stream()
-
-
 
 if __name__ == "__main__":
     main()
